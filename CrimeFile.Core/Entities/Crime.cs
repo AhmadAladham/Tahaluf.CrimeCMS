6e566126294172320bb6f51db8f0b5ef96de981d
Ahmad Aladham <aladhamahmad588@gmail.com>
2021-06-22 21:49:37 +0300

    nullable

--- CrimeFile.Core/Entities/Crime.cs
+++ CrimeFile.Core/Entities/Crime.cs
@@ -15,9 +15,9 @@ namespace CrimeFile.Core.Entities
         public DateTime CrimeDate { get; set; }
         public bool IsClosed { get; set; }
         public string Location { get; set; }
-        public string CrimeDescription { get; set; }
+        public string? CrimeDescription { get; set; }
         public DateTime CloseDate { get; set; }
-        public string CriminalDescription { get; set; }
-        public string Image { get; set; }
+        public string? CriminalDescription { get; set; }
+        public string? Image { get; set; }
     }
 }
